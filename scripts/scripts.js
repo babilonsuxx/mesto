8aee85c8360cce9da95d66c0962358f690fe0834
babilonsuxx@gmail.com <babilonsuxx@gmail.com>
2020-10-01 04:02:50 +0300

    all in

---
+++ scripts/scripts.js
@@ -0,0 +1,47 @@
+
+let editBtn=document.querySelector('.profile__edit-btn');
+let popup=document.querySelector('.popup');
+let popupCloseBtn=document.querySelector('.popup__btn-close');
+let popupFormEdit=document.querySelector('.popup__form');
+
+let name=document.querySelector('.profile__name');
+let job=document.querySelector('.profile__job');
+let nameInput=document.querySelector('.popup__filed-name');
+let jobInput=document.querySelector('.popup__field-job');
+
+let popupToggle=() => {
+  popup.classList.toggle('popup_is-open');
+}
+
+let onClickEditBtn=()=> {
+  nameInput.value=name.innerText;
+  jobInput.value=job.innerText;
+  popupToggle();
+}
+
+let onClickPopubCloseBtn=()=> {
+  popupToggle();
+}
+
+let onClickPopupBackground=(event)=> {
+  if(event.target===event.currentTarget) {
+    popupToggle();
+  }
+}
+
+let popupFormSubmit=(event)=> {
+  event.preventDefault();
+  name.innerText=nameInput.value;
+  job.innerText=jobInput.value;
+  popupToggle();
+}
+
+
+editBtn.addEventListener('click', onClickEditBtn);
+popupCloseBtn.addEventListener('click', onClickPopubCloseBtn);
+popupFormEdit.addEventListener('submit', popupFormSubmit);
+popup.addEventListener('click', onClickPopupBackground);
+
+
+
+
