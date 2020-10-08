8aec41ceee43ba846cc2067df1c41b7b5e9e3b20
babilonsuxx@gmail.com <babilonsuxx@gmail.com>
2020-10-09 00:59:16 +0300

    feat: add edit to popup

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,29 +1,29 @@
 
 let editBtn=document.querySelector('.profile__edit-btn');
-let popup=document.querySelector('.popup');
-let popupCloseBtn=document.querySelector('.popup__btn-close');
-let popupFormEdit=document.querySelector('.popup__form');
+let popup=document.querySelector('.popup-edit');
+let popupCloseBtn=document.querySelector('.popup-edit__btn-close');
+let popupFormEdit=document.querySelector('.popup-edit__form');
 
 let name=document.querySelector('.profile__name');
 let job=document.querySelector('.profile__job');
-let nameInput=document.querySelector('.popup__filed_name');
-let jobInput=document.querySelector('.popup__field_job');
+let nameInput=document.querySelector('.popup-edit__filed_name');
+let jobInput=document.querySelector('.popup-edit__field_job');
 
 
 
 let onClickEditBtn=()=> {
   nameInput.value=name.innerText;
   jobInput.value=job.innerText;
-  popup.classList.add('popup_is-open');
+  popup.classList.add('popup-edit_is-open');
 }
 
 let onClickCloseBtn=()=> {
-  popup.classList.remove('popup_is-open');
+  popup.classList.remove('popup-edit_is-open');
 }
 
 let onClickPopupBackground=(event)=> {
   if(event.target===event.currentTarget) {
-    popup.classList.remove('popup_is-open');
+    popup.classList.remove('popup-edit_is-open');
   }
 }
 
