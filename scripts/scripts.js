e33c955f751f01664dbdcba3934a6dd00da0e01b
babilonsuxx@gmail.com <babilonsuxx@gmail.com>
2020-10-13 01:11:43 +0300

    feat: from data to DOM

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,32 @@
+const initialCards =[
+  {
+   name: 'Москва',
+   link: './images/moscow.jpg'
+  },
+  {
+    name: 'Дубаи',
+    link: './images/dubai.jpg'
+  },
+  {
+    name: 'Лондон',
+    link: './images/london.jpg'
+  },
+  {
+    name: 'Нью-Йорк',
+    link: './images/new-york.jpg'
+  },
+  {
+    name: 'Сингапур',
+    link: './images/singapore.jpg'
+  },
+  {
+    name: 'Токио',
+    link: './images/tokio.jpg'
+  }
+];
+const elements=document.querySelector('.elements');
+
+const templateElement=document.querySelector('.template__element');
 
 let editBtn=document.querySelector('.profile__edit-btn');
 let popup=document.querySelector('.popup-edit');
@@ -8,6 +37,24 @@ let name=document.querySelector('.profile__name');
 let job=document.querySelector('.profile__job');
 let nameInput=document.querySelector('.popup-edit__filed_name');
 let jobInput=document.querySelector('.popup-edit__field_job');
+
+// рисуем 6 из массива
+const renderElements=()=>{
+  const items=initialCards.map(element=>getElements(element));
+  elements.append(...items);
+}
+
+// генерим 1 для того, чтобы его поставить куда нить.
+const getElements=(data)=>{
+  const element=templateElement.content.cloneNode(true);
+  element.querySelector('.element__img').setAttribute('alt', data.name);
+  element.querySelector('.element__img').setAttribute('src', data.link);
+  element.querySelector('.element__title').innerText=data.name;
+  return element;
+}
+
+
+
 
 
 
@@ -41,5 +88,5 @@ popupFormEdit.addEventListener('submit', popupFormSubmit);
 popup.addEventListener('click', onClickPopupBackground);
 
 
-
+renderElements();
 
